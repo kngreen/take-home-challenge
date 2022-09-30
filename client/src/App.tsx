9425d2828f1358f3e4922965b8face49c9ca6e2b
Ali Alobaidi <alinawar2000@gmail.com>
2022-09-30 10:30:48 -0400

    remove some styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 import './assets/stylesheets/styles.scss'
 import '../src/assets/webfonts/Nunito/Nunito-Black.ttf'
-import '../node_modules/react-grid-layout/css/styles.css'
 
 const App = () => {
-  return <p>Good Luck!</p>
+  return <p style={{ color: 'white' }}>Good Luck!</p>
 }
 
 export default App
