91131e2d222da4a771044f524a1944649721a85d
kngreen <kngreen@umich.edu>
2022-10-05 17:20:41 -0400

    button blow

--- client/src/components/elements/CitySelectorBox.tsx
+++ client/src/components/elements/CitySelectorBox.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 const CitySelectorBox = () => {
   return (
     <div>
+      {/* <video playsinline autoplay muted loop>
+        <source src="../../assets/images/goldconfetti" type="video/webm"></source>
+        Your browser does not support the video tag.
+      </video> */}
       <h1 className='CitySelector-prompt'>WHERE ARE YOU LOOKING FOR EXPERIENCES?</h1>
       <div className='CitySelector-cities'>
-        <button className='CitySelector-cities-gold'><span>🗽</span>New York</button>
-        <button className='CitySelector-cities-gold'><span>🌴</span>Miami</button>
-        <button className='CitySelector-cities-gold'><span>☀️</span>Los Angeles</button>
-        <button className='CitySelector-cities-teal'><span>📍</span>Near Me</button>
+        <button className='CitySelector-cities-button'><span>🗽</span>New York</button>
+        <button className='CitySelector-cities-button'><span>🌴</span>Miami</button>
+        <button className='CitySelector-cities-button'><span>☀️</span>Los Angeles</button>
+        <button className='CitySelector-cities-button-teal'><span>📍</span>Near Me</button>
       </div>
     </div>
   )
