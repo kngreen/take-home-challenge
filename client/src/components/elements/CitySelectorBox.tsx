6a772262436308b6ca96e23300633765036f3388
kngreen <kngreen@umich.edu>
2022-10-05 16:18:23 -0400

    navbar buttons

--- client/src/components/elements/CitySelectorBox.tsx
+++ client/src/components/elements/CitySelectorBox.tsx
@@ -2,21 +2,13 @@ import React from 'react'
 
 const CitySelectorBox = () => {
   return (
-    <div className='CitySelector'>
+    <div>
       <h1 className='CitySelector-prompt'>WHERE ARE YOU LOOKING FOR EXPERIENCES?</h1>
       <div className='CitySelector-cities'>
-        <div className='gold'>
-          <button onClick={() => console.log('click!')}>New York</button>
-        </div>
-        <div className='gold'>
-          <button>Miami</button>
-        </div>
-        <div className='gold'>
-          <button>Los Angeles</button>
-        </div>
-        <div>
-          <button>Near Me</button>
-        </div>
+        <button className='CitySelector-cities-gold'><span>🗽</span>New York</button>
+        <button className='CitySelector-cities-gold'><span>🌴</span>Miami</button>
+        <button className='CitySelector-cities-gold'><span>☀️</span>Los Angeles</button>
+        <button className='CitySelector-cities-teal'><span>📍</span>Near Me</button>
       </div>
     </div>
   )
